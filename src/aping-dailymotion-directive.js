424386e91b90683e28f4ce32a445ea8f0ba21eb9
Jonathan Hornung <jonathan.hornung@gmail.com>
2016-03-12 11:39:31 +0100

    style: removed blank lines

--- src/aping-dailymotion-directive.js
+++ src/aping-dailymotion-directive.js
@@ -7,24 +7,19 @@ angular.module("jtt_aping_dailymotion", ['jtt_dailymotion'])
             restrict: 'A',
             replace: 'false',
             link: function (scope, element, attrs, apingController) {
-
                 var appSettings = apingController.getAppSettings();
-
                 var requests = apingUtilityHelper.parseJsonFromAttributes(attrs.apingDailymotion, apingDailymotionHelper.getThisPlattformString(), appSettings);
 
                 requests.forEach(function (request) {
-
                     //create helperObject for helper function call
                     var helperObject = {
                         model: appSettings.model,
                     };
-
                     if (angular.isDefined(appSettings.getNativeData)) {
                         helperObject.getNativeData = appSettings.getNativeData;
                     } else {
                         helperObject.getNativeData = false;
                     }
-
                     if (request.protocol === "http" || request.protocol === "https") {
                         helperObject.protocol = request.protocol + "://";
                     } else  if (appSettings.protocol === "http" || appSettings.protocol === "https") {
@@ -32,47 +27,36 @@ angular.module("jtt_aping_dailymotion", ['jtt_dailymotion'])
                     } else {
                         helperObject.protocol = "//";
                     }
-
                     //create requestObject for api request call
                     var requestObject = {};
-
                     if (angular.isDefined(request.items)) {
                         requestObject.limit = request.items;
                     } else {
                         requestObject.limit = appSettings.items;
                     }
-
                     if (requestObject.limit === 0 || requestObject.limit === '0') {
                         return false;
                     }
-
                     // -1 is "no explicit limit". same for NaN value
                     if (requestObject.limit < 0 || isNaN(requestObject.limit)) {
                         requestObject.limit = undefined;
                     }
-
                     // the api has a limit of 100 items per request
                     if (requestObject.limit > 100) {
                         requestObject.limit = 100;
                     }
-
                     if (angular.isDefined(request.search)) {
                         requestObject.search = request.search;
                     }
-
                     if (angular.isDefined(request.tags)) {
                         requestObject.tags = request.tags;
                     }
-
                     if (request.userId) {
-
                         requestObject.id = request.userId;
                         requestObject.sort = 'recent';
-
                         if (angular.isDefined(request.channelId)) {
                             requestObject.channel = request.channelId;
                         }
-
                         dailymotionFactory.getVideosFromUserById(requestObject)
                             .then(function (_data) {
                                 if (_data) {
@@ -80,10 +64,8 @@ angular.module("jtt_aping_dailymotion", ['jtt_dailymotion'])
                                 }
                             });
                     } else if (request.channelId) {
-
                         requestObject.id = request.channelId;
                         requestObject.sort = 'recent';
-
                         dailymotionFactory.getVideosFromChannelById(requestObject)
                             .then(function (_data) {
                                 if (_data) {
@@ -91,10 +73,8 @@ angular.module("jtt_aping_dailymotion", ['jtt_dailymotion'])
                                 }
                             });
                     } else if (request.playlistId) {
-
                         requestObject.id = request.playlistId;
                         requestObject.sort = 'recent';
-
                         dailymotionFactory.getVideosFromPlaylistById(requestObject)
                             .then(function (_data) {
                                 if (_data) {
@@ -102,19 +82,15 @@ angular.module("jtt_aping_dailymotion", ['jtt_dailymotion'])
                                 }
                             });
                     } else {
-
                         if (angular.isDefined(request.genre)) {
                             requestObject.genre = request.genre;
                         }
-
                         if (angular.isDefined(request.country)) {
                             requestObject.country = request.country;
                         }
-
                         if (angular.isDefined(request.language)) {
                             requestObject.detected_language = request.language;
                         }
-
                         dailymotionFactory.getVideosByParams(requestObject)
                             .then(function (_data) {
                                 if (_data) {
@@ -122,7 +98,6 @@ angular.module("jtt_aping_dailymotion", ['jtt_dailymotion'])
                                 }
                             });
                     }
-
                 });
             }
         }
